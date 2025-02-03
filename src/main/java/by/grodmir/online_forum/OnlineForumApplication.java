efd57788b2e17577cbcb827dfcd371b539dff511
grodmir <dionislobacevich@gmail.com>
2025-02-03 20:26:24 +0300

    Project initialize.

---
+++ src/main/java/by/grodmir/online_forum/OnlineForumApplication.java
@@ -0,0 +1,13 @@
+package by.grodmir.online_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineForumApplication.class, args);
+	}
+
+}
